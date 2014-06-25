507505b4e86669f1500737c6cce6cdc71212e960
archit90 <a.gupta.archit@gmail.com>
2014-06-25 10:28:15 +0530

    changed ButtonKeys

--- app/src/main/java/com/friedapps/scientycalc/calculator/ButtonKeys.java
+++ app/src/main/java/com/friedapps/scientycalc/calculator/ButtonKeys.java
@@ -6,6 +6,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ButtonKeys {
+
+    public static class KeyDetail {
+        private int kLength;
+        private String kSymbol;
+
+        public KeyDetail(int kLength, String kSymbol) {
+            this.kLength = kLength;
+            this.kSymbol = kSymbol;
+        }
+
+        public int getLength() {
+            return kLength;
+        }
+
+        public String getSymbol() {
+            return kSymbol;
+        }
+    }
+
     public enum Key {
         k0, k1, k2, k3, k4, k5, k6, k7, k8, k9, kDot,
         kAdd, kSubt, kMul, kDiv, kExp, kMod,
@@ -14,18 +33,26 @@ public class ButtonKeys {
     }
 
     private static Map<Integer, Key> keyMap = new HashMap<Integer, Key>();
-    private static Map<Key, String> keySymbol = new HashMap<Key, String>();
+    private static Map<Key, KeyDetail> keyDetailMap = new HashMap<Key, KeyDetail>();
 
     public static Key getKey(int id) {
         return keyMap.get(id);
     }
 
     public static String getKeySymbol(int id) {
-        return keySymbol.get(getKey(id));
+        return keyDetailMap.get(getKey(id)).getSymbol();
     }
 
     public static String getKeySymbol(Key k) {
-        return keySymbol.get(k);
+        return keyDetailMap.get(k).getSymbol();
+    }
+
+    public static int getKeyLength(int id) {
+        return keyDetailMap.get(getKey(id)).getLength();
+    }
+
+    public static int getKeyLength(Key k) {
+        return keyDetailMap.get(k).getLength();
     }
 
     static {
@@ -56,32 +83,36 @@ public class ButtonKeys {
         keyMap.put(R.id.btnCos, Key.kCos);
         keyMap.put(R.id.btnTan, Key.kTan);
         keyMap.put(R.id.btnLog, Key.kLog);
-        // TODO: sin cos tan log
     }
 
     static {
-        keySymbol.put(Key.k0, "0");
-        keySymbol.put(Key.k1, "1");
-        keySymbol.put(Key.k2, "2");
-        keySymbol.put(Key.k3, "3");
-        keySymbol.put(Key.k4, "4");
-        keySymbol.put(Key.k5, "5");
-        keySymbol.put(Key.k6, "6");
-        keySymbol.put(Key.k7, "7");
-        keySymbol.put(Key.k8, "8");
-        keySymbol.put(Key.k9, "9");
-        keySymbol.put(Key.kDot, ".");
-        keySymbol.put(Key.kPlusMinus, "-");
+        keyDetailMap.put(Key.k0, new KeyDetail(1,"0"));
+        keyDetailMap.put(Key.k1, new KeyDetail(1,"1"));
+        keyDetailMap.put(Key.k2, new KeyDetail(1,"2"));
+        keyDetailMap.put(Key.k3, new KeyDetail(1,"3"));
+        keyDetailMap.put(Key.k4, new KeyDetail(1,"4"));
+        keyDetailMap.put(Key.k5, new KeyDetail(1,"5"));
+        keyDetailMap.put(Key.k6, new KeyDetail(1,"6"));
+        keyDetailMap.put(Key.k7, new KeyDetail(1,"7"));
+        keyDetailMap.put(Key.k8, new KeyDetail(1,"8"));
+        keyDetailMap.put(Key.k9, new KeyDetail(1,"9"));
+        keyDetailMap.put(Key.kDot, new KeyDetail(1,"."));
+        keyDetailMap.put(Key.kPlusMinus, new KeyDetail(1,"-"));
 
-        keySymbol.put(Key.kAdd, "+");
-        keySymbol.put(Key.kSubt, "-");
-        keySymbol.put(Key.kMul, "*");
-        keySymbol.put(Key.kDiv, "/");
-        keySymbol.put(Key.kMod, "%");
-        keySymbol.put(Key.kExp, "^");
+        keyDetailMap.put(Key.kAdd, new KeyDetail(1,"+"));
+        keyDetailMap.put(Key.kSubt, new KeyDetail(1,"-"));
+        keyDetailMap.put(Key.kMul, new KeyDetail(1,"*"));
+        keyDetailMap.put(Key.kDiv, new KeyDetail(1,"/"));
+        keyDetailMap.put(Key.kMod, new KeyDetail(1,"%"));
+        keyDetailMap.put(Key.kExp, new KeyDetail(1,"^"));
 
-        keySymbol.put(Key.kBrOpenClose, "");
-        keySymbol.put(Key.kAns, "=");
+        keyDetailMap.put(Key.kBrOpenClose, new KeyDetail(1,""));
+        keyDetailMap.put(Key.kAns, new KeyDetail(1,"="));
+
+        keyDetailMap.put(Key.kSin, new KeyDetail(3,"Sin"));
+        keyDetailMap.put(Key.kCos, new KeyDetail(3,"Cos"));
+        keyDetailMap.put(Key.kTan, new KeyDetail(3,"Tan"));
+        keyDetailMap.put(Key.kLog, new KeyDetail(3,"Log"));
 
     }
 }
